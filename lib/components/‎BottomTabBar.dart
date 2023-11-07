e728d1406ae484f6d7cfaa113ffdd0c27ba60d21
mohammed roshdy <mohamed.roshdy.twfyq@gmail.com>
2023-11-07 12:18:27 +0200

    e-commerce implement

---
+++ lib/components/‎BottomTabBar.dart
@@ -0,0 +1,44 @@
+class BottomTabBar extends StatefulWidget {
+  BottomTabBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomTabBar> createState() => _BottomTabBarState();
+}
+
+class _BottomTabBarState extends State<BottomTabBar> {
+  int _index = 0;
+  final screens = [
+    HomePage(),
+    SearchPage(),
+    ProfilePage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: screens[_index],
+
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _index,
+            onTap: (value) {
+              setState(() {
+                _index = value;
+              });
+            },
+            
+            backgroundColor: Color.fromARGB(255, 227, 227, 227),
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Boxicons.bx_home_circle),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Boxicons.bx_search),
+                label: 'Search',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Boxicons.bxs_user),
+                label: 'Profile',
+              )
+            ]));
+  }
+}
